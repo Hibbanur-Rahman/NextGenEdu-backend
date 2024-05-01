d842f7b404949fb252b7ef6afd5f110542d225fb
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-05-02 03:37:08 +0530

    fixing the view review list controller

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -7,7 +7,7 @@ const { ViewTeacherDetails, UpdateTeacherDetails } = require("../controller/teac
 const upload = require("../middleware/multerMiddleware");
 const { AddCourse,ViewCourses, ViewPublishCourseByTeacher, ViewCourseDetailByID, EnrolledCourseByStudentId,ViewEnrolledStudentId } = require("../controller/courseController");
 const { AddWishlist, ViewWishlistDetails } = require("../controller/wishlistController");
-const { AddReview } = require("../controller/reviewController");
+const { AddReview, ViewReviewListByCourseId } = require("../controller/reviewController");
 
 
 Router.post("/register", register);
@@ -46,5 +46,6 @@ Router.post('/view-enrolled-by-studentID',verifyToken,ViewEnrolledStudentId);
 
 //reviews routes
 Router.post('/add-review',verifyTokenNew,AddReview);
+Router.post('/view-Review-courseId',ViewReviewListByCourseId);
 
 module.exports = Router;
